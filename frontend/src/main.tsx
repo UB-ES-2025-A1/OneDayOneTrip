16ae53c91dd0f6e5f24772c54a812ba1237ab269
lidiabm <lbudioma7@alumnes.ub.edu>
2025-10-15 20:59:52 +0200

    Eliminats Login.tsx i Registre.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,19 +5,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './styles/styles.css';
 
 import Home from "./pages/Home";
-import Register from "./components/Register";
-import Login from "./components/Login";
 import Dashboard from "./pages/Dashboard";
-
-
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
