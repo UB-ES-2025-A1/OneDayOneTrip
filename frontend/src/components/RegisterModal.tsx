4f4d52fd1006fb617264109d8380a3994e0d1db5
ILikeDonetes <pablosuperviapages@gmail.com>
2025-10-20 20:04:05 +0200

    After correctly registering, the register modal now closes itself and opens the login modal.

--- frontend/src/components/RegisterModal.tsx
+++ frontend/src/components/RegisterModal.tsx
@@ -42,10 +42,8 @@ export default function RegisterModal({ onClose, openLogin }: RegisterProps) {
 
       alert("Usuari registrat correctament");
 
-      // Puedes cerrar el modal o redirigir directamente
       onClose();
-      navigate("/login");
-      navigate("/");
+      openLogin();
     } catch (err: any) {
       setError(err.message);
     } finally {
