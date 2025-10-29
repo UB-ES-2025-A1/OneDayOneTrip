ddd12110b0601a318ac8e1351ce4ce5a4b5e2460
eliagarcia <elia.garcia.rovira@gmail.com>
2025-10-29 16:41:16 +0100

    canvis

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { onAuthStateChanged, signOut, type User } from "firebase/auth";
 import { auth } from "../firebase.ts";
 import "../styles/Home.css";
