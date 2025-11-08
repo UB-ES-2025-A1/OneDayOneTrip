3d019f95ac3d0319848f014eed1f4a289444c232
Martin <mmarchrovira@gmail.com>
2025-11-08 12:02:05 +0100

    microservicio levantado

--- api/app/services/image_service.py
+++ api/app/services/image_service.py
@@ -8,16 +8,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 IMGBB_API_KEY = os.getenv("IMGBB_API_KEY")
-IMGBB_ALBUM_ID = os.getenv("IMGBB_ALBUM_ID")
 
 def upload_image_to_imgbb(file: UploadFile):
     url = "https://api.imgbb.com/1/upload"
     image_data = base64.b64encode(file.file.read())
     payload = {
         "key": IMGBB_API_KEY,
-        "image": image_data,
-        "album": IMGBB_ALBUM_ID  # 👈 aquí va el álbum destino
+        "image": image_data
     }
     response = requests.post(url, data=payload)
     response.raise_for_status()
     return response.json()["data"]["url"]
+
+
+#Microservicio imgBB
