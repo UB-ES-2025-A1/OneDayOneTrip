542cbba49c84ccd725e97d75af87d737868cde16
ILikeDonetes <pablosuperviapages@gmail.com>
2025-11-12 04:47:00 +0100

    rating_model kept as original

--- api/app/models/rating_model.py
+++ api/app/models/rating_model.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel, Field
-from datetime import datetime, UTC
-
+from datetime import datetime
 
 class RatingModel(BaseModel):
     userId: str
     rating: int = Field(ge=1, le=5)
-    date: datetime = datetime.now(UTC)
+    date: datetime = datetime.utcnow()
