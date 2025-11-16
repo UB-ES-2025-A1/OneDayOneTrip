8b9d154d594bc66b479485b02910895d7ff67fa9
quico16 <quico1607@gmail.com>
2025-11-16 17:36:42 +0100

    Endpoint deixar de seguir

--- api/app/routers/users.py
+++ api/app/routers/users.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from datetime import datetime
+from google.cloud import firestore
 
 from app.services.firebase_service import db
 from app.auth.verify_token import verify_token
@@ -72,3 +73,31 @@ async def get_current_user_by_id(user_id: str):
     if not doc.exists:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return doc.to_dict()
+
+@router.post("/unfollow/{user_id}/{target_id}")
+async def unfollow_user(user_id: str, target_id: str):
+    user_ref = db.collection("users").document(user_id)
+    target_ref = db.collection("users").document(target_id)
+
+    user_doc = user_ref.get()
+    target_doc = target_ref.get()
+
+    # Comprovem que l'usuari existeix
+    if not user_doc.exists:
+        raise HTTPException(status_code=404, detail="L'usuari que intenta deixar de seguir no existeix")
+
+    # Comprovem que existeix l'usuari a deixar de seguir
+    if not target_doc.exists:
+        raise HTTPException(status_code=404, detail="L'usuari que vols deixar de seguir no existeix")
+
+    # L'eliminem de la llista de seguits
+    user_ref.update({
+        "llista_seguits": firestore.ArrayRemove([target_id])
+    })
+
+    # L'eliminem de la llista de seguidors
+    target_ref.update({
+        "llista_seguidors": firestore.ArrayRemove([user_id])
+    })
+
+    return {"message": "Has deixat de seguir l'usuari correctament"}
