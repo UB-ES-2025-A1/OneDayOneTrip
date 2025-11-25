a5f7bbdc650cf7a34126ba5aa1d701ea039e49bc
ILikeDonetes <pablosuperviapages@gmail.com>
2025-11-26 00:56:40 +0100

    Trigger tests

--- frontend/src/__tests__/RutaDetall.test.tsx
+++ frontend/src/__tests__/RutaDetall.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, waitFor, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
-//Comment to trigger changes
+//Comment to trigger changesaoeu
 // Mocks hoisted
 const {
   mockGetTripById,
