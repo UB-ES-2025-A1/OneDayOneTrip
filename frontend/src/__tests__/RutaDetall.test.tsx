6cdcdc6b34177337440255df4592a7d22048f964
ILikeDonetes <pablosuperviapages@gmail.com>
2025-11-26 00:49:12 +0100

    Linting solving

--- frontend/src/__tests__/RutaDetall.test.tsx
+++ frontend/src/__tests__/RutaDetall.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, waitFor, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
-
+//Comment to trigger changes
 // Mocks hoisted
 const {
   mockGetTripById,
