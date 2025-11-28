0c7bab1a84de070b7211fe09d9ba43fde712e6d9
eliagarcia <elia.garcia.rovira@gmail.com>
2025-11-28 08:55:06 +0100

    butons ja be

--- frontend/src/pages/RutaDetall.tsx
+++ frontend/src/pages/RutaDetall.tsx
@@ -261,33 +261,28 @@ export default function RutaDetall() {
         <div className="ruta-header-line">
           <h1 className="ruta-titol">{tripData.title}</h1>
   
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "1fr auto",
-              alignItems: "center",
-              justifyContent: "space-between",
-              gap: "10px",
-            }}
-          >
+          <div className="ruta-actions">            
             {/* Esquerra: Rating + Valorar */}
-            <div style={{display: "flex", width: "150%"}}>
+            <div className="ruta-actions-left">
               {tripData.avgRating != null && (
-                <div className="rating-summary">
-                  <span className="rating-star">★</span>
-                  <span className="rating-value">{tripData.avgRating.toFixed(1)}</span>
-                  <span className="rating-count">({tripData.numRatings})</span>
-                </div>
-              )}
-              <div style={{display: "flex", gap: "10px"}}>
-              <button
-                className="valorar-button"
-                onClick={() => setShowRatingModal(true)}
-              >
-                <span className="valorar-icon">★</span>
-                Valorar
-              </button>
-
+              <div className="rating-summary">
+                <span className="rating-star">★</span>
+                <span className="rating-value">
+                  {tripData.avgRating.toFixed(1)}
+                </span>
+                <span className="rating-count">
+                  ({tripData.numRatings})
+                </span>
+              </div>
+            )}
+          <div className="ruta-actions-buttons">
+            <button
+              className="valorar-button"
+              onClick={() => setShowRatingModal(true)}
+            >
+              <span className="valorar-icon">★</span>
+              Valorar
+            </button>
             {/* Dreta: Botó Guardar */}
             <button
               type="button"
@@ -383,7 +378,7 @@ export default function RutaDetall() {
         />
       </div>
 
-      {/* 🔥 Component de comentaris */}
+      {/*comentaris */}
       <Comments 
         tripId={tripData._id!} 
         currentUser={currentUser} 
