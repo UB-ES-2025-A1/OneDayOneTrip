b98711a0ab6d8be57cac7cdfcfb5f0b1e166e820
eliagarcia <elia.garcia.rovira@gmail.com>
2025-12-05 11:10:07 +0100

    eliminar compte fet

--- frontend/src/components/Valorar.tsx
+++ frontend/src/components/Valorar.tsx
@@ -11,8 +11,8 @@ export default function Valorar({ onClose, onSubmit }: ValorarProps) {
   const [rating, setRating] = useState(0);
 
   const handleSubmit = () => {
-    onSubmit(rating);  // 👈 llamamos al padre
-    onClose();         // 👈 cerramos modal
+    onSubmit(rating); 
+    onClose();        
   };
 
   const starElements: JSX.Element[] = [];
